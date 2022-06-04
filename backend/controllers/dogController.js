2cf815cdde68f51ef7da3a33a1526286ce4828a7
Joshua Perez Leduc <joshuapleduc@gmail.com>
2022-06-04 19:43:15 -0400

    create front, models, and controllers

--- backend/controllers/dogController.js
+++ backend/controllers/dogController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
+const Dog = require('../models/dogModel')
+const {nanoid} = require('nanoid')
 
 // @desc  Get Dogs
 // @route GET /api/dogs
 // @access Private
 const getDogs = asyncHandler( async (req,res) => {
-    res.status(200).json({message: 'getDogs'})
+    const dogs = await Dog.find()
+    res.status(200).json(dogs)
 })
 
 // @desc  set Dog
@@ -14,25 +17,41 @@ const setDog = asyncHandler( async (req,res) => {
     if(!req.body.text){
      throw new Error("Please add text")
     }
+
+    const dog = await Dog.create(req.body)
         
-    res.status(200).json({message: 'getDogs'})
+    res.status(200).json(dog)
 })
 
-// @desc  Get Dogs
-// @route GET /api/dogs
+// @desc  Update Dog
+// @route PUT /api/dog
 // @access Private
-const getDog = asyncHandler( async (req,res) => {
-    res.status(200).json({message: 'getDogs'})
+const updateDog = asyncHandler( async (req,res) => {
+    const dog = await Dog.findById(req.params.id)
+    if(!dog){
+        res.status(400)
+        throw new Error('Dog not found')
+    }
+    const updatedDog=await Dog.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedDog)
 })
 
 // @desc  Get Dogs
 // @route GET /api/dogs
 // @access Private
 const deleteDog = asyncHandler( async (req,res) => {
-    res.status(200).json({message: 'getDogs'})
+    const dog = await Dog.findById(req.params.id)
+    if(!dog){
+        res.status(400)
+        throw new Error('Dog not found')
+    }
+    const deletedDog=await Dog.findByIdAndDelete(req.params.id)
+    res.status(200).json(deletedDog)
 })
 
 module.exports = {
     getDogs,
-    setDog
+    setDog,
+    updateDog, 
+    deleteDog
 }
